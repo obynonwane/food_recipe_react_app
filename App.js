fb649a4ac2364a77e846b64e640f3600c648b97d
obynonwane <biostechnologyng@gmail.com>
2024-08-13 20:37:06 +0100

    Getting Started with the React Navigation Package

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <CategoriesScreen />
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
