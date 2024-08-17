244a7e7addbd2b66c0d5cac70e344638a9af0e27
obynonwane <biostechnologyng@gmail.com>
2024-08-17 12:23:19 +0100

    Managing App-Wide State with Context

--- store/context/favorites-context.js
+++ store/context/favorites-context.js
@@ -1,5 +1,5 @@
 // 1. create context
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 //2. create a context object - with method s and variable it will use - exported since we will use it somewher else
 export const FavoritesContext = createContext({
@@ -13,7 +13,31 @@ export const FavoritesContext = createContext({
 // interact with this context
 function FavoritesContextProvider({ children }) {
   //4. the actual implementation of the above context object will be here
-  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>;
+  // that will manage all the variables and methods in the context object
+
+  //create state to manege the meals id
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
+  function removeFavorite(id) {
+    // filter out the one not needed
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
+
+  // then pass our data/state into the context provider
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 }
 
 // 5. export
